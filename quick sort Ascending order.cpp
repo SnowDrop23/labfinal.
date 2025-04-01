0080cb36bcb3c3ad5f4004b1d0f4968e803fce1c
toma <144379705+SnowDrop23@users.noreply.github.com>
2025-04-01 11:10:37 +0600

    Create quick sort Ascending order.cpp

---
+++ quick sort Ascending order.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int n)
+{
+	cout << "After sorting: ";
+	for(int i = 0; i < n; i++) cout << arr[i] << " ";
+}
+
+int partition(int arr[], int lb, int ub)
+{
+	int pivot = arr[lb];
+	int start = lb;
+	int end = ub;
+
+	while(start < end)
+	{
+		while(arr[start] <= pivot) start++;
+		while(arr[end] > pivot) end--;
+
+		if(start < end) swap(arr[start], arr[end]);
+	}
+	swap(arr[lb], arr[end]);
+	return end;
+}
+
+void quickSort(int arr[], int lb, int ub)
+{
+	if(lb < ub)
+	{
+		int location = partition(arr, lb, ub);
+
+		quickSort(arr, lb, location-1);
+		quickSort(arr, location+1, ub);
+	}
+}
+
+int main()
+{
+	int n;
+	cout << "Enter the size of array: ";
+	cin >> n;
+
+	int arr[n];
+	cout << "Enter array elements: ";
+	for(int i = 0; i < n; i++) cin >> arr[i];
+	
+	quickSort(arr, 0, n-1);
+	printArray(arr, n);
+}
